3a1437c9d4410665f9361e25c8756d419bd53291
James Hughes <jh13293@my.bristol.ac.uk>
2015-07-09 16:57:07 +0100

    removed fetch.js from jhint

--- Gruntfile.js
+++ Gruntfile.js
@@ -9,13 +9,13 @@ module.exports = function(grunt) {
     var artifactsLocation = "build_artifacts";
     grunt.initConfig({
         jshint: {
-            all: ["Gruntfile.js", "server.js", "server/**/*.js", "test/**/*.js", "public/**/*.js"],
+            all: ["Gruntfile.js", "server.js", "server/**/*.js", "test/**/*.js", "public/**/*.js", "!public/fetch.js"],
             options: {
                 jshintrc: true
             }
         },
         jscs: {
-            all: ["Gruntfile.js", "server.js", "server/**/*.js", "test/**/*.js", "public/**/*.js"]
+            all: ["Gruntfile.js", "server.js", "server/**/*.js", "test/**/*.js", "public/**/*.js", "!public/fetch.js"]
         },
         mochaTest: {
             test: {
